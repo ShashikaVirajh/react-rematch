c9c9958598ba87d32aa7edd2b697d3acee092557
Shashika Virajh <shashikasvka@gmail.com>
2023-04-06 21:41:19 +0530

    Removed unused code

--- src/store/models/root.model.ts
+++ src/store/models/root.model.ts
@@ -1,9 +1,5 @@
 import { Models } from '@rematch/core';
-import loadingPlugin, { ExtraModelsFromLoading } from '@rematch/loading';
-import persistPlugin from '@rematch/persist';
 import { cocktailStore } from './cocktail.model';
-
-
 
 export interface RootModel extends Models<RootModel> {
   cocktailStore: typeof cocktailStore
