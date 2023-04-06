8a3c8dcd135373015edcb05c34dcaed21986d324
Shashika Virajh <shashikasvka@gmail.com>
2023-04-06 08:57:42 +0530

    Removed unwanted code

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,5 @@
 import { RematchDispatch, RematchRootState, init } from '@rematch/core';
 import loadingPlugin, { ExtraModelsFromLoading } from '@rematch/loading';
-import {
-  TypedUseSelectorHook,
-  useDispatch as useReduxDispatch,
-  useSelector as useReduxSelector,
-} from 'react-redux';
 
 import { RootModel, models } from './models/root.model';
 
